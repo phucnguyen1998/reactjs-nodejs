ec45097d683b8b8bfd0da058bdc55f6754429918
phucnguyen1998 <phucnguyen11011998@gmail.com>
2020-03-27 08:01:23 +0700

    [1.0.0] connect db

--- back-end/product_manager_api/routes/index.js
+++ back-end/product_manager_api/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sanpham',
+  password: 'admin',
+  port: 5432,
+})
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  pool.query('SELECT NOW()', (err, res) => {
+    //console.log(err, res)
+    pool.end()
+  })
   res.render('index', { title: 'Express' });
 });
+
+// api get data from postgres sql
+router.get('/getdata', function(req, res) {
+  console.log('day la api lay dl');
+  // get data
+  pool.query('select * from product_info', (err, response) => {
+    console.log(response.rows)
+    pool.end()
+  })
+  res.render('getdata');
+});
+
 
 module.exports = router;
